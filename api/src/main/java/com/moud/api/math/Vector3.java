33e4d175fb64d0f4a5f29d668c352444d25818c2
Meekiavelique <meekiavelique@gmail.com>
2025-09-18 21:09:51 +0200

    feat(api): new multiply method

--- api/src/main/java/com/moud/api/math/Vector3.java
+++ api/src/main/java/com/moud/api/math/Vector3.java
@@ -1,13 +1,18 @@
 package com.moud.api.math;
 
 public class Vector3 {
-    public float x;
-    public float y;
-    public float z;
+    public double x, y, z;
 
     public Vector3() {
-        this(0.0f, 0.0f, 0.0f);
+        this(0, 0, 0);
     }
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
 
     public Vector3(float x, float y, float z) {
         this.x = x;
@@ -15,26 +20,20 @@ public class Vector3 {
         this.z = z;
     }
 
-    public Vector3(double x, double y, double z) {
-        this.x = (float) x;
-        this.y = (float) y;
-        this.z = (float) z;
-    }
-
-    public static Vector3 zero()
-    {
-        return new Vector3(0f, 0f, 0f);
-    }
     public Vector3 add(Vector3 other) {
-        return new Vector3(x + other.x, y + other.y, z + other.z);
+        return new Vector3(this.x + other.x, this.y + other.y, this.z + other.z);
     }
 
-    public Vector3 multiply(float scalar) {
-        return new Vector3(x * scalar, y * scalar, z * scalar);
+    public Vector3 multiply(double scalar) {
+        return new Vector3(this.x * scalar, this.y * scalar, this.z * scalar);
+    }
+
+    public static Vector3 zero() {
+        return new Vector3(0, 0, 0);
     }
 
     @Override
     public String toString() {
-        return "Vector3(" + x + ", " + y + ", " + z + ")";
+        return "Vector3{" + "x=" + x + ", y=" + y + ", z=" + z + '}';
     }
 }
