6f65846c44f28a1b5533e9109090b2a1c1e24084
mrqsdf <maqw@free.fr>
2025-12-08 01:02:56 +0100

    Add prevention in ModelData

--- plugin-api/src/main/java/com/moud/plugin/api/models/ModelData.java
+++ plugin-api/src/main/java/com/moud/plugin/api/models/ModelData.java
@@ -1,9 +1,17 @@
 package com.moud.plugin.api.models;
 
+import java.util.Objects;
+
 public record ModelData(String modelPath, String texturePath, String modelId) {
 
     public ModelData(String modelPath, String texturePath){
         this(modelPath, texturePath, modelPath);
+    }
+
+    public ModelData {
+        Objects.requireNonNull(modelPath, "modelPath cannot be null");
+        texturePath = texturePath == null ? "" : texturePath;
+        modelId = modelId == null ? modelPath : modelId;
     }
 
 }
