153c746d9946724c1203de14534478bbc908e4a5
Meekiavelique <meekiavelique@gmail.com>
2025-12-21 14:49:15 +0100

    feat(server): getters for scene camera

--- server/src/main/java/com/moud/server/camera/SceneCamera.java
+++ server/src/main/java/com/moud/server/camera/SceneCamera.java
@@ -1,6 +1,7 @@
 package com.moud.server.camera;
 
 import com.moud.api.math.Vector3;
+import org.graalvm.polyglot.HostAccess;
 
 public record SceneCamera(
         String id,
@@ -11,4 +12,38 @@ public record SceneCamera(
         double nearPlane,
         double farPlane
 ) {
+    @HostAccess.Export
+    public String getId() {
+        return this.id;
+    }
+
+    @HostAccess.Export
+    public String getLabel() {
+        return this.label;
+    }
+
+    @HostAccess.Export
+    public Vector3 getPosition() {
+        return this.position;
+    }
+
+    @HostAccess.Export
+    public Vector3 getRotation() {
+        return this.rotation;
+    }
+
+    @HostAccess.Export
+    public double getFov() {
+        return this.fov;
+    }
+
+    @HostAccess.Export
+    public double getNearPlane() {
+        return this.nearPlane;
+    }
+
+    @HostAccess.Export
+    public double getFarPlane() {
+        return this.farPlane;
+    }
 }
