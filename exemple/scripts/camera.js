242ac4d608b9a2a32ae642489c926180ad687fbe
Meek <154077770+Meekiavelique@users.noreply.github.com>
2026-03-27 10:24:43 +0100

    Delete exemple directory

--- exemple/scripts/camera.js
+++
@@ -1,6 +0,0 @@
-  ({                                                                                      
-    _process(api, dt) {                                                                                                                                                                                                                    
-      // localX=0 (center), localY=2.5 (above), localZ=-5 (behind), pitch=-15°, roll=0                                                                                                                                                     
-      api.setFollowCamera(0, 2.5, 5, -15, 0);                                                                                                                                                                                             
-    }                                                                                                                                                                                                                                      
-  })                            
