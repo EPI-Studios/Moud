49b9498777e0ac5dc1685585077813b78e9d4d54
Meekiavelique <meekiavelique@gmail.com>
2026-04-12 16:21:13 +0200

    feat(client): add editor diagnostics and canvas node types

---
+++ client-fabric/src/main/java/com/moud/client/fabric/editor/panels/CanvasNodeTypes.java
@@ -0,0 +1,23 @@
+package com.moud.client.fabric.editor.panels;
+
+import java.util.Set;
+
+final class CanvasNodeTypes {
+    static final Set<String> CANVAS_2D_TYPES = Set.of(
+            "Node2D", "Camera2D",
+            "CanvasItem", "Control", "CanvasLayer",
+            "HBoxContainer", "VBoxContainer", "GridContainer",
+            "MarginContainer", "ScrollContainer", "PanelContainer",
+            "Label", "RichTextLabel", "TextureRect", "AnimatedTextureRect", "ColorRect", "ProgressBar",
+            "Button", "TextureButton", "CheckBox", "HSlider", "VSlider", "LineEdit");
+
+    static final Set<String> CONTROL_TYPES = Set.of(
+            "CanvasItem", "Control",
+            "HBoxContainer", "VBoxContainer", "GridContainer",
+            "MarginContainer", "ScrollContainer", "PanelContainer",
+            "Label", "RichTextLabel", "TextureRect", "AnimatedTextureRect", "ColorRect", "ProgressBar",
+            "Button", "TextureButton", "CheckBox", "HSlider", "VSlider", "LineEdit");
+
+    private CanvasNodeTypes() {
+    }
+}
