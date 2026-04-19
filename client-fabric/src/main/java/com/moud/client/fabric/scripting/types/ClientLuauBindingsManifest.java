4211be54538f43dd04bb6586f26d080a948da58c
Meekiavelique <meekiavelique@gmail.com>
2026-04-19 21:31:14 +0200

    feat(client): generate Luau type definitions and bindings at startup

---
+++ client-fabric/src/main/java/com/moud/client/fabric/scripting/types/ClientLuauBindingsManifest.java
@@ -0,0 +1,11 @@
+package com.moud.client.fabric.scripting.types;
+
+
+public final class ClientLuauBindingsManifest {
+
+    private ClientLuauBindingsManifest() {}
+
+    public static void registerAll() {
+        //
+    }
+}
