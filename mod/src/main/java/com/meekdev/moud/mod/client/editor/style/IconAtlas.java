56d94943b651aa5ffbba444b14bdf5c23e545633
Meekiavelique <meekiavelique@gmail.com>
2026-09-15 10:40:43 +0200

    refresh editor icon ids every frame

--- mod/src/main/java/com/meekdev/moud/mod/client/editor/style/IconAtlas.java
+++ mod/src/main/java/com/meekdev/moud/mod/client/editor/style/IconAtlas.java
@@ -4,6 +4,7 @@ import com.meekdev.moud.mod.MoudMod;
 import com.mojang.blaze3d.platform.NativeImage;
 import foundry.imgui.api.ImGuiMC;
 import foundry.imgui.api.ImGuiTextureProvider;
+import foundry.imgui.impl.ImGuiMCImpl;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.EnumMap;
@@ -16,17 +17,18 @@ public final class IconAtlas {
 
     public long textureId(EditorIcon icon) {
         ImGuiTextureProvider provider = textures.computeIfAbsent(icon, IconAtlas::upload);
-        return provider == null ? 0L : provider.imguimc$id();
+        return ImGuiMCImpl.handler.getRenderer().getImGuiId(provider, null);
     }
 
     private static ImGuiTextureProvider upload(EditorIcon icon) {
+        NativeImage image;
         try (InputStream in = IconAtlas.class.getResourceAsStream(icon.resourcePath())) {
             if (in == null) throw new IOException("missing icon " + icon.resourcePath());
-            DynamicTexture texture = new DynamicTexture(() -> "moud editor icon " + icon.name(), NativeImage.read(in));
-            return ImGuiMC.getTexture(texture);
+            image = NativeImage.read(in);
         } catch (IOException e) {
             MoudMod.LOG.error("editor icon {} could not be loaded", icon.resourcePath(), e);
-            return null;
+            image = new NativeImage(1, 1, true);
         }
+        return ImGuiMC.getTexture(new DynamicTexture(() -> "moud editor icon " + icon.name(), image));
     }
 }
