f511df218292623de7da5ab7e58ddd585db88949
Meekiavelique <meekiavelique@gmail.com>
2026-09-17 19:56:22 +0200

    keep module source on the server unless shared

--- core/src/main/java/com/meekdev/moud/core/script/ModuleScript.java
+++ core/src/main/java/com/meekdev/moud/core/script/ModuleScript.java
@@ -1,5 +1,6 @@
 package com.meekdev.moud.core.script;
 
+import com.meekdev.moud.core.asset.Res;
 import com.meekdev.moud.core.clazz.Prop;
 import com.meekdev.moud.core.instance.Instance;
 
@@ -8,4 +9,17 @@ public final class ModuleScript extends Instance {
     @Prop(asset = true) public String source = "";
 
     public String code = "";
+
+    public boolean forClients() {
+        if (source.startsWith(Res.SCHEME + "client/") || source.startsWith(Res.SCHEME + "shared/")) return true;
+        for (Instance up = parent(); up != null; up = up.parent()) {
+            if (up instanceof LocalScript) return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected long externalPropertyMask() {
+        return forClients() ? 0 : 1L << def().property("code").index();
+    }
 }
