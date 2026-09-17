5194ee64ef36dd660215a44a11c6b79f927fa8b7
Meekiavelique <meekiavelique@gmail.com>
2026-09-17 20:09:19 +0200

    send module code from replicated storage only

--- core/src/main/java/com/meekdev/moud/core/script/ModuleScript.java
+++ core/src/main/java/com/meekdev/moud/core/script/ModuleScript.java
@@ -3,6 +3,10 @@ package com.meekdev.moud.core.script;
 import com.meekdev.moud.core.asset.Res;
 import com.meekdev.moud.core.clazz.Prop;
 import com.meekdev.moud.core.instance.Instance;
+import com.meekdev.moud.core.service.ReplicatedStorage;
+import com.meekdev.moud.core.service.StarterCharacterScripts;
+import com.meekdev.moud.core.service.StarterGui;
+import com.meekdev.moud.core.service.StarterPlayerScripts;
 
 public final class ModuleScript extends Instance {
 
@@ -11,11 +15,14 @@ public final class ModuleScript extends Instance {
     public String code = "";
 
     public boolean forClients() {
-        if (source.startsWith(Res.SCHEME + "client/") || source.startsWith(Res.SCHEME + "shared/")) return true;
         for (Instance up = parent(); up != null; up = up.parent()) {
-            if (up instanceof LocalScript) return true;
+            if (up.serverOnly()) return false;
+            if (up instanceof LocalScript || up instanceof ReplicatedStorage || up instanceof StarterGui
+                    || up instanceof StarterPlayerScripts || up instanceof StarterCharacterScripts) {
+                return true;
+            }
         }
-        return false;
+        return source.startsWith(Res.SCHEME + "client/") || source.startsWith(Res.SCHEME + "shared/");
     }
 
     @Override
